a84a4a37c5b279a105aa268df25a751f5411729d
Дмитрий <dimabogolepov2002@mail.ru>
2025-07-31 18:27:26 +0300

    feat:added component for articles. Main page visual done for 70%

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -5,6 +5,7 @@ import { LoaderComponent } from './components/loader/loader.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import {RouterModule, RouterOutlet} from "@angular/router";
+import { ArticleComponent } from './components/article/article.component';
 
 
 
@@ -13,10 +14,12 @@ import {RouterModule, RouterOutlet} from "@angular/router";
     LayoutComponent,
     LoaderComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ArticleComponent
   ],
   exports: [
-    LoaderComponent
+    LoaderComponent,
+    ArticleComponent
   ],
   imports: [
     CommonModule,
