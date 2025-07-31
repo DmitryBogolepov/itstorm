496be476eb0bcb9f7127cbe12d9a6ea0118e0fe5
Дмитрий <dimabogolepov2002@mail.ru>
2025-07-31 18:27:26 +0300

    feat:added component for articles. Main page visual done for 70%

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {LayoutComponent} from "./shared/layout/layout.component";
+import {MainComponent} from "./views/main/main.component";
 
 const routes: Routes = [
   {
     path: '', component: LayoutComponent,
     children: [
-    //   {path: '', component: MainComponent},
-    {path:'', loadChildren: () => import('./views/user/user.module').then(m => m.UserModule)},
-    //   {path:'', loadChildren: () => import('./views/product/product.module').then(m => m.ProductModule)},
-    //   {path:'', loadChildren: () => import('./views/order/order.module').then(m => m.OrderModule)},
-    //   {path:'', loadChildren: () => import('./views/personal/personal.module').then(m => m.PersonalModule),canActivate:[AuthGuard]},
+      {path: '', component: MainComponent},
+      {path: '', loadChildren: () => import('./views/user/user.module').then(m => m.UserModule)},
+      //   {path:'', loadChildren: () => import('./views/product/product.module').then(m => m.ProductModule)},
+      //   {path:'', loadChildren: () => import('./views/order/order.module').then(m => m.OrderModule)},
+      //   {path:'', loadChildren: () => import('./views/personal/personal.module').then(m => m.PersonalModule),canActivate:[AuthGuard]},
     ]
   }
 ];
@@ -19,4 +20,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import {SharedModule} from "./shared/shared.module";
+import { MainComponent } from './views/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
