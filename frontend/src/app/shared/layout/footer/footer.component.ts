ae2cb92089ee53a1391303d356dbb7e008c61216
Дмитрий <dimabogolepov2002@mail.ru>
2025-08-02 17:26:45 +0300

    feat:added comment component for future pages. added modal for footer

--- frontend/src/app/shared/layout/footer/footer.component.ts
+++ frontend/src/app/shared/layout/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
+  showSuccess = false;
+  isShowed = false;
+
+  footerOrderForm = this.fb.group({
+    name:['',[Validators.required]],
+    number:['',[Validators.required]]
+  })
 
   ngOnInit(): void {
   }
 
+  modalShow() {
+    this.isShowed = !this.isShowed;
+  }
+
+  submitOrder() {
+    this.showSuccess =  !this.showSuccess;
+  }
 }
