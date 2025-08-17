81430fb9a312dbd04acc7dc484cd12aa8f66c1ad
Дмитрий <dimabogolepov2002@mail.ru>
2025-08-17 13:48:35 +0300

    feat:added comments requests for article page.added gap to comment cards. added input logic in app-comments.

--- frontend/src/app/shared/components/comment/comment.component.ts
+++ frontend/src/app/shared/components/comment/comment.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {CommentType} from "../../../../types/comments.type";
 
 @Component({
   selector: 'app-comment',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
-
+  @Input() comment!:CommentType;
   constructor() { }
 
   ngOnInit(): void {
--- frontend/src/app/views/blog/blog-page/blog-page.component.ts
+++ frontend/src/app/views/blog/blog-page/blog-page.component.ts
@@ -7,6 +7,9 @@ import {DefaultResponseType} from "../../../../types/default-response.type";
 import {ArticleService} from "../../../shared/services/article.service";
 import {Article, ArticleType} from "../../../../types/articles.type";
 import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
+import {CommentsService} from "../../../shared/services/comments.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {CommentsType} from "../../../../types/comments.type";
 
 @Component({
   selector: 'app-blog-page',
@@ -15,30 +18,35 @@ import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 })
 export class BlogPageComponent implements OnInit {
 
-  constructor(private fb:FormBuilder,private authService:AuthService,private activatedRoute:ActivatedRoute,private articlesService:ArticleService,private sanitizer: DomSanitizer) { }
+  constructor(private _snackBar:MatSnackBar,private commentsService:CommentsService,private fb:FormBuilder,private authService:AuthService,private activatedRoute:ActivatedRoute,private articlesService:ArticleService,private sanitizer: DomSanitizer) { }
   isLogged = false;
+  articleUrl!:string;
   commentForm = this.fb.group({
-    text:[''],
+    message:[''],
     article:['']
   })
   safeText!: SafeHtml;
+  comments:CommentsType = {
+    allCount:0,
+    comments:[]
+  }
   article:ArticleType | null= null;
   relatedArticles:Article[] | null = null;
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
-
-      this.articlesService.getArticle(params['url']).subscribe((data:ArticleType) => {
-        this.article = data;
-        if (this.article?.text) {
-          this.safeText = this.sanitizer.bypassSecurityTrustHtml(this.article.text);
-        }
-      });
-      this.articlesService.getRelatedArticles(params['url']).subscribe((data:Article[]) => {
-        this.relatedArticles = data;
-      })
+      this.articleUrl = params['url']
     })
-
-
+    this.articlesService.getArticle(this.articleUrl ).subscribe((data:ArticleType) => {
+      this.article = data;
+      this.comments.allCount = data.commentsCount;
+      this.comments.comments = data.comments;
+      if (this.article?.text) {
+        this.safeText = this.sanitizer.bypassSecurityTrustHtml(this.article.text);
+      }
+    });
+    this.articlesService.getRelatedArticles(this.articleUrl ).subscribe((data:Article[]) => {
+      this.relatedArticles = data;
+    })
 
     this.authService.isLogged$.subscribe((isLogged: boolean) => {
       this.isLogged = isLogged;
@@ -59,6 +67,22 @@ export class BlogPageComponent implements OnInit {
 
 
   sendComment() {
+      if (this.commentForm.valid && this.article) {
+        this.commentsService.sendComment(this.commentForm.controls['message'].value!, this.article.id).subscribe((data:DefaultResponseType) => {
+          if (data.error) {
+            this._snackBar.open(data.message)
+            throw new Error(data.message);
+          } else {
+            this._snackBar.open('Комментарий успешно отправлен!')
+          }
+          this.commentForm.clearValidators()
+        })
+      }
+      if (this.article) {
+        this.commentsService.getComments(this.article.id).subscribe(data => {
+          this.comments = data;
+        })
+      }
 
   }
 }
